07d0d46e2d96782c80798676b9458f05bc8a3626
Critzvolt <72713966+Critzvolt@users.noreply.github.com>
2023-03-27 16:12:22 +0200

    .

---
+++ src/main/java/com/critzcrm/crm/data/repositories/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.critzcrm.crm.data.repositories;
+
+import com.critzcrm.crm.data.entities.Customer;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
+}
